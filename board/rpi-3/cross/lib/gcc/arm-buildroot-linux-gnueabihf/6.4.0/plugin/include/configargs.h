1b453d1e5785c1743b105fe297d76ad0e714727c
anistairi <anis_tairi@outlook.com>
2018-12-02 15:32:27 +0100

    init

---
+++ board/rpi-3/cross/lib/gcc/arm-buildroot-linux-gnueabihf/6.4.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --prefix=/home/think/br-tree/buildroot-2018.02.7/../board/rpi-3/cross --sysconfdir=/home/think/br-tree/buildroot-2018.02.7/../board/rpi-3/cross/etc --enable-static --target=arm-buildroot-linux-gnueabihf --with-sysroot=/home/think/br-tree/buildroot-2018.02.7/../board/rpi-3/cross/arm-buildroot-linux-gnueabihf/sysroot --disable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --with-gmp=/home/think/br-tree/buildroot-2018.02.7/../board/rpi-3/cross --with-mpc=/home/think/br-tree/buildroot-2018.02.7/../board/rpi-3/cross --with-mpfr=/home/think/br-tree/buildroot-2018.02.7/../board/rpi-3/cross --with-pkgversion='Buildroot 2018.02.7' --with-bugurl=http://bugs.buildroot.net/ --disable-libquadmath --enable-tls --disable-libmudflap --enable-threads --without-isl --without-cloog --disable-decimal-float --with-abi=aapcs-linux --with-cpu=cortex-a53 --with-fpu=neon-vfpv4 --with-float=hard --with-mode=arm --enable-languages=c,c++ --with-build-time-tools=/home/think/br-tree/buildroot-2018.02.7/../board/rpi-3/cross/arm-buildroot-linux-gnueabihf/bin --enable-shared --disable-libgomp";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "aapcs-linux" }, { "cpu", "cortex-a53" }, { "float", "hard" }, { "mode", "arm" }, { "fpu", "neon-vfpv4" }, { "tls", "gnu" } };
